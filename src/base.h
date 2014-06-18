1dd70346c53a4305ec5513f6471bcc0c4d2ff392
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2014-06-18 09:02:04 -0300

    Base file.

---
+++ src/base.h
@@ -0,0 +1,35 @@
+#if defined(__APPLE__) || defined(MACOSX)
+    #include <OpenGL/gl.h>
+    #include <GLUT/glut.h>
+#elif defined(WIN32)
+    #include <windows.h>
+    #include <GL/gl.h>
+    #include <GL/glut.h>
+#else
+    #include <GL/gl.h>
+    #include <GL/glut.h>
+#endif
+
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+// Objects can navigate from -5.0f to 5.0f.
+#define BOARD_SIZE 5.25f
+// Y axis difference. Ground to Objects.
+#define DIFF       0.25f
+
+enum Direction
+{
+    DOWN  = 1,
+    UP    = 2,
+    LEFT  = 3,
+    RIGHT = 4,
+};
+
+struct Point
+{
+    float x, y, z;
+};
